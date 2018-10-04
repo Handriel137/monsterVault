42eed88d3f1dd582df45226f04f8500a39c91fb0
Handriel137 <r.solnik@gmail.com>
2018-10-04 09:45:18 -0400

    uncommented migration

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,9 +1,4 @@
 class Monster < ApplicationRecord
     has_and_belongs_to_many :special_ability
 
-    def self.randomMonster
-        
-        
-    end
-    
 end
--- db/migrate/20180926014639_create_join_table_monsters_special_abilities.rb
+++ db/migrate/20180926014639_create_join_table_monsters_special_abilities.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableMonstersSpecialAbilities < ActiveRecord::Migration[5.2]
   def change
     create_join_table :monster, :special_ability  do |t|
-      # t.belongs_to :monster, index: true
-      # t.belongs_to :special_ability, index: true
+      t.belongs_to :monster, index: true
+      t.belongs_to :special_ability, index: true
     end
   end
 end
